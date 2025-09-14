3ae228be843ebc4b69b1a6603c47ae8e9405bc83
Rohail Tabassum <rohailtabassumit@gmail.com>
2025-09-14 22:20:00 +0500

    Non-conflicting changes with merge

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ print("Hello World") # prints hello world on console
 print("More changes on 14th September 2025")
 
 print("Making changes for this branch")
+
+print("This is a non-conflicting change in branch feature_2. Will use merge")
